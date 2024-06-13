adf3c6d462ec9eb1047bd42e8b0e8a423dc861bf
strygwyr76 <401027847@qq.com>
2024-06-14 00:00:21 +0800

    scripts

---
+++ scripts_mi/train_gqe_FB15k_baseline.sh
@@ -0,0 +1,11 @@
+CUDA_VISIBLE_DEVICES=2 python ../model/train_baseline_privacy.py \
+    --train_query_dir ../input_files_privacy/FB15K_train_queries.pkl \
+    --valid_query_dir ../input_files_privacy/FB15K_valid_queries.pkl \
+    --test_query_dir ../input_files_privacy/FB15K_test_private_public_queries.pkl \
+    --test_private_query_dir ../input_files_privacy/FB15K_private_1p_queries.json \
+    --data_name FB15K \
+    --model gqe \
+    --batch_size 1024 \
+    --log_steps 60000 \
+    --mi \
+    --privacy_multiplier 0.05
